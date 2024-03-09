d8d13b458ec4c9d9ca547fe0f3975442df382afb
Gordon Lin <g.lin02@hotmail.com>
2024-03-09 04:01:43 +0000

    Initial setup of react app

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 Gordon Lin. Licensed under the Apache License, Version 2.0.</p>
+    </footer>
+  );
+}
+
+export default Footer;
